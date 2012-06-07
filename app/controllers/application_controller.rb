f3bf80a261ed45b298578b0520f437156cda8f94
Arley Lobato <arleylobato@gmail.com>
2012-06-07 17:28:06 -0300

    authorize

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :authorize
+
+  private
+
+  def authorize
+    unless Rails.env.test?
+      authenticate_or_request_with_http_basic do |username, password|
+        username == ENV['HTTP_USER'] && password == ENV['HTTP_PASS']
+      end
+    end
+  end
 
 end
